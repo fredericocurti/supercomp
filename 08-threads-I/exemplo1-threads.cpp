89eecbcf5118a9b5d4881168998418d6a72fe95f
vilela69 <frederico.vilelacurti@gmail.com>
2019-09-03 23:32:19 -0300

    added aula 8

--- 08-threads-I/exemplo1-threads.cpp
+++ 08-threads-I/exemplo1-threads.cpp
@@ -1,27 +1,30 @@
 #include <thread>
 #include <iostream>
 
-void funcao_rodando_em_paralelo(int a, int *b) {
-    std::cout << "a=" << a << std::endl;
+
+void funcao_rodando_em_paralelo(int a, int *b, int tid) {
+    std::cout << "Thread: " << tid << "a=" << a << std::endl;
     *b = 5;
 }
 
-
 int main() {
     int b = 10;
-
-    // Cria thread e a executa.
-    // Primeiro argumento é a função a ser executada.
+    int tid = 0;
+    unsigned concurentThreadsSupported = std::thread::hardware_concurrency();
+    std::thread *threads = new std::thread[concurentThreadsSupported];
+    std::cout << "Threads supported:" << concurentThreadsSupported << std::endl;
     
     // Os argumentos em seguida são passados diretamente
     // para a função passada no primeiro argumento.
-    std::thread t1(funcao_rodando_em_paralelo, 15, &b);
-    
-    
-    std::cout << "Antes do join b=" << b << std::endl;
-    
-    // Espera até que a função acabe de executar.
-    t1.join();
+    for (int i = 0; i < concurentThreadsSupported; i++) {
+        threads[i] = std::thread(funcao_rodando_em_paralelo, 15, &b, tid);
+        tid++;
+        std::cout << "Antes do join b=" << b << std::endl;
+    }
+
+    for (int i = 0; i < concurentThreadsSupported; i++) {
+        threads[i].join();
+    }
     
     std::cout << "Depois do join b=" << b << std::endl;
 }
--- 08-threads-I/pi-numeric-integration.cpp
+++ 08-threads-I/pi-numeric-integration.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
 #include <chrono>
+#include <thread>
 
 static long num_steps = 100000000;
 double step;
+
+void paralel_sum(double *sum, int start, int end) {
+    int i;
+    double x = 0, sum_local = 0;
+
+    for (i = start; i < end; i++) {
+        x = (i + 0.5) * step;
+        sum_local = sum_local + 4.0 / (1.0 + x * x);
+    }
+
+    *sum += sum_local;
+}
+
 int main() {
     int i;
     double x, pi, sum = 0.0;
+    int N_threads = 4;
+    int spt = num_steps / N_threads; // steps per thread
+    std::thread threads[4];
     step = 1.0 / (double)num_steps;
 
     auto start_time = std::chrono::high_resolution_clock::now();
-    for (i = 0; i < num_steps; i++) {
-        x = (i + 0.5) * step;
-        sum = sum + 4.0 / (1.0 + x * x);
+
+    // Spawn threads
+    for (int i = 0; i < N_threads; i++) {
+        threads[i] = std::thread(paralel_sum, &sum, i * spt, (i+1) * spt);
     }
 
+    // Wait for them to finish
+    for (int i = 0; i < N_threads; i++) {
+        threads[i].join();
+    }
+
+    // Calculate pi
     pi = step * sum;
     auto end_time = std::chrono::high_resolution_clock::now();
-    auto runtime = std::chrono::duration_cast<std::chrono::seconds> (end_time - start_time);
+    auto runtime = std::chrono::duration_cast<std::chrono::milliseconds> (end_time - start_time);
     
     std::cout << "O valor de pi calculado com " << num_steps << " passos levou ";
-    std::cout << runtime.count() << " segundo(s) e chegou no valor : ";
+    std::cout << runtime.count() << " milisegundo(s) e chegou no valor : ";
     std::cout.precision(17);
     std::cout << pi << std::endl;
 }
