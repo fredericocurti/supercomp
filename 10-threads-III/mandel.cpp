47bc4a5bcd0e99ebc7457c95d530c4a780c6ec98
vilela69 <frederico.vilelacurti@gmail.com>
2019-09-10 11:00:19 -0300

    exericios threads done

--- 10-threads-III/mandel.cpp
+++ 10-threads-III/mandel.cpp
@@ -3,51 +3,51 @@
 #include <math.h>
 #include <omp.h>
 
-# define NPOINTS 1000
-# define MAXITER 10000
+#define NPOINTS 1000
+#define MAXITER 10000
 
-void testpoint(void);
-
-struct d_complex{
+struct d_complex {
    double r;
    double i;
 };
 
-struct d_complex c;
-int numoutside = 0;
-
-void testpoint(void){
-   struct d_complex z;
+int testpoint(struct d_complex c) {
    int iter;
    double temp;
-   z=c;
-   for (iter=0; iter<MAXITER; iter++){
-      temp = (z.r*z.r)-(z.i*z.i)+c.r;
-      z.i = z.r*z.i*2+c.i;
+   struct d_complex z = c;
+
+   for (iter = 0; iter < MAXITER; iter++) {
+      temp = (z.r * z.r) - (z.i * z.i) + c.r;
+      z.i = z.r * z.i * 2 + c.i;
       z.r = temp;
-      if ((z.r*z.r+z.i*z.i)>4.0) {
-        numoutside++;
-        break;
+      if ((z.r * z.r + z.i * z.i) > 4.0) {
+         return 1;
       }
    }
+   return 0;
 }
 
-
-int main(){
+int main() {
    int i, j;
-   double area, error, eps  = 1.0e-5;
-   #pragma omp parallel for default(shared)
-    for (i=0; i<NPOINTS; i++) {
-        for (j=0; j<NPOINTS; j++) {
-            c.r = -2.0+2.5*(double)(i)/(double)(NPOINTS)+eps;
-            c.i = 1.125*(double)(j)/(double)(NPOINTS)+eps;
-            testpoint();
-        }
-    }
-   
-   area=2.0*2.5*1.125*(double)(NPOINTS*NPOINTS-numoutside)/(double)(NPOINTS*NPOINTS);
-   error=area/(double)NPOINTS;
-   printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n",area,error);
+   double area, error;
+   double eps = 1.0e-5;
+   struct d_complex c;
+   int numoutside = 0;
+
+   #pragma omp parallel for private(c)
+   for (i = 0; i < NPOINTS; i++) {
+      #pragma omp parallel for
+      for (j = 0; j < NPOINTS; j++) {
+         c.r = -2.0 + 2.5 * (double)(i) / (double)(NPOINTS) + eps;
+         c.i = 1.125 * (double)(j) / (double)(NPOINTS) + eps;
+         #pragma omp atomic
+         numoutside += testpoint(c);
+      }
+   }
+
+   area = 2.0 * 2.5 * 1.125 * (double)(NPOINTS * NPOINTS - numoutside) / (double)(NPOINTS * NPOINTS);
+   error = area / (double)NPOINTS;
+   printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n", area, error);
    printf("Correct answer should be around 1.510659\n");
    printf("numoutside: %d\n", numoutside);
 }
